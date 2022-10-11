e2a3d4dad484305feb7dcb3e4d8cceb2b346a0f8
jg22dc8 <jolien.goossens@ugent.be>
2022-10-11 07:25:36 +0200

    Added network analysis scripts

---
+++ src/models/Network_CA.R
@@ -0,0 +1,77 @@
+##############################################
+# Network analysis: Correspondence Analysis  #
+##############################################
+
+# Jolien Goossens - Marine Biology Research Group, Ghent University (Marbiol) / Flanders Marine Institute (VLIZ)
+# R version 3.6.2
+
+
+#### Read data and load packages ####
+source("src/backfun/read_and_format.R")
+library(FactoMineR)
+
+df_pres = read_csv("data/interim/df_pres.csv")
+
+#### Format ####
+# Receiver at station ZOKN was moved to ZW1 (very close): consider as 1 station for this analysis
+df_pres = df_pres %>% mutate(station_name = ifelse(station_name == "bpns-ZOKN", "bpns-ZW1", station_name))
+
+
+# Bring to daily level
+df_pres_an = df_pres %>% 
+  group_by(station_name, animal_id, date) %>% 
+  summarise(det_bin = ifelse(sum(det_bin) == 0, 0, 1))
+
+# Calculate period at large and RI (= res)
+df_pres_an = df_pres_an %>% 
+  group_by(animal_id, station_name) %>% 
+  summarise(n_date = length(unique(date)),
+            n_det = sum(det_bin)) %>% 
+  mutate(res = n_det/n_date)
+
+# Make matrix
+df_pres_an = df_pres_an %>% 
+  select(animal_id, station_name, res) %>% 
+  spread(animal_id, res, fill = 0) 
+
+
+#### CA animal - station ####
+clust = df_pres_an %>% 
+  data.matrix()
+row.names(clust) = df_pres_an$station_name
+clust = clust[,-1]
+cari = CA(clust)
+
+#### Plot output CA  ####
+coor_an = as.data.frame(cari$col$coord[,1:2])
+colnames(coor_an) = c("dim1", "dim2")
+coor_an$animal_id = as.numeric(row.names(coor_an))
+coor_an = an %>% select(animal_id, station_group) %>% right_join(coor_an)
+
+coor_stat = as.data.frame(cari$row$coord[,1:2])
+colnames(coor_stat) = c("dim1", "dim2")
+coor_stat$station_name = row.names(coor_stat)
+coor_stat = deploy_zeeb %>% select(station_name, label_name, station_group) %>% right_join(coor_stat)
+
+plot_ca = ggplot() +
+  theme_bw() +
+  theme(legend.position = "none") +
+  coord_fixed(ratio = 1/2) +
+  geom_point(data = coor_stat, shape = 8, size = 5, alpha = 0.8, aes(dim1, dim2, colour = station_group)) +
+  geom_point(data = coor_an, shape = 21, size = 2, aes(dim1, dim2, fill = station_group)) +
+  geom_text(data = filter(coor_stat, label_name %in% c("LNG", "ZA2")), 
+            aes(dim1, dim2, label = label_name, colour = station_group),
+            position = position_nudge(x = -0.1)) +
+  geom_text(data = filter(coor_an, animal_id %in% c(7179, 9089, 3511)), 
+            aes(dim1, dim2, label = animal_id, colour = station_group),
+            position = position_nudge(y = 0.2, x = -0.1)) +
+  scale_fill_manual(values = cols, name = "Location") +
+  scale_colour_manual(values = cols, name = "Location") +
+  labs(x = paste0("Dim 1 - ", round(cari$eig[1,2], 2), "%"),
+       y = paste0("Dim 2 - ", round(cari$eig[2,2], 2), "%"))
+
+
+#### Save plot #### 
+ggsave(filename = "reports/figures/Fig4_CA.jpg", 
+       plot = plot_ca,   
+       scale = 1, dpi = 600, width = 8, height = 14, units = "cm")
---
+++ src/models/Network_spatial.R
@@ -0,0 +1,179 @@
+##################################
+# Network analysis: spatial map  #
+##################################
+
+# Jolien Goossens - Marine Biology Research Group, Ghent University (Marbiol) / Flanders Marine Institute (VLIZ)
+# R version 3.6.2
+
+
+#### Run read_and_format.R ####
+source("src/backfun/read_and_format.R")
+source("src/backfun/base_map.R")
+source("src/backfun/sharkov_stehfest2015.R")
+library(patchwork)
+
+#### Plot for all stations concatenating port wall stations ####
+#### Filter Zeebrugge ####
+df = df %>% 
+  filter(acoustic_project_code == "bpns") %>%  # only consider Zeebrugge detections (not release)
+  filter(station_group != 'Other')  # only consider Zeebrugge detections
+
+#### Get edges ####
+deploy_zeeb2 <- deploy_zeeb %>%
+  mutate(
+    deploy_latitude = case_when(
+      station_name %in% c("bpns-zbw1", "bpns-zbw2", "bpns-zbw3") ~ 51.36291,
+      station_name %in% c("bpns-zbe1", "bpns-zbe2", "bpns-zbe3") ~ 51.3643,
+      station_name %in% c("bpns-ZW1", "bpns-ZOKN") ~ 51.35821,
+      TRUE ~ deploy_latitude),
+    deploy_longitude = case_when(
+      station_name %in% c("bpns-zbw1", "bpns-zbw2", "bpns-zbw3") ~ 3.183634,
+      station_name %in% c("bpns-zbe1", "bpns-zbe2", "bpns-zbe3") ~ 3.200701,
+      station_name %in% c("bpns-ZW1", "bpns-ZOKN") ~ 3.189648,
+      TRUE ~ deploy_longitude),
+    station_name = case_when(
+      station_name %in% c("bpns-zbw1", "bpns-zbw2", "bpns-zbw3") ~ "bpns-zbw",
+      station_name %in% c("bpns-zbe1", "bpns-zbe2", "bpns-zbe3") ~ "bpns-zbe",
+      station_name %in% c("bpns-ZW1", "bpns-ZOKN") ~ "bpns-ZW1",
+      TRUE ~ station_name))
+
+df_zeeb <- df %>% 
+  select(animal_id, station_name, date_time) %>% 
+  mutate(
+    station_name = case_when(
+      station_name %in% c("bpns-zbw1", "bpns-zbw2", "bpns-zbw3") ~ "bpns-zbw",
+      station_name %in% c("bpns-zbe1", "bpns-zbe2", "bpns-zbe3") ~ "bpns-zbe",
+      station_name %in% c("bpns-ZW1", "bpns-ZOKN") ~ "bpns-ZW1",
+      TRUE ~ station_name))  %>% 
+  left_join(deploy_zeeb2)
+
+
+df_short <- df_zeeb %>% 
+  arrange(date_time) %>% 
+  group_by(animal_id) %>% 
+  mutate(llon=lead(deploy_longitude), llat=lead(deploy_latitude), lstation = lead(station_name), lstation_group = lead(station_group)) %>% 
+  filter(llon != deploy_longitude) %>% 
+  group_by(animal_id, 
+           station_name, station_group, deploy_longitude, deploy_latitude, 
+           lstation, lstation_group, llon, llat) %>% 
+  summarise(n=n()) %>% 
+  group_by(station_name, station_group, deploy_longitude, deploy_latitude, 
+           lstation, lstation_group, llon, llat) %>% 
+  summarise(n = sum(n))
+
+#### Format for sharkov ####
+df_sharkov = df %>% 
+  mutate(station_name = case_when(
+    station_name %in% c("bpns-zbw1", "bpns-zbw2", "bpns-zbw3") ~ "bpns-zbw",
+    station_name %in% c("bpns-zbe1", "bpns-zbe2", "bpns-zbe3") ~ "bpns-zbe",
+    station_name %in% c("bpns-ZW1", "bpns-ZOKN") ~ "bpns-ZW1",
+    TRUE ~ station_name)) %>% 
+  select(
+    id = animal_id,
+    time = date_hour,
+    state = station_name) %>% 
+  group_by(id, time, state) %>% 
+  dplyr::summarise() %>% 
+  as.data.frame()
+
+#### Calculate centrality ####
+sharkov_result_eigen <- sharkov(df_sharkov, return.matrix = FALSE, time_res = "1 hour", include_absence = FALSE)
+sharkov_result_df = data.frame(
+  station_name = rownames(sharkov_result_eigen),
+  sharkov_result_eigen = round(sharkov_result_eigen, digits = 2)
+)
+sharkov_result_df = sharkov_result_df %>% left_join(deploy_zeeb2)
+
+#### Plot map ####
+network_map = base_map +
+  theme(axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        panel.grid = element_blank(),
+        plot.margin = unit(c(0,0,0,0), "cm")) +
+  theme(legend.position=c(0.15, 0.15),
+        legend.title = element_blank(),
+        panel.border = element_rect(colour = "black", fill = NA)) +
+  geom_curve(data = df_short, 
+             aes(x=deploy_longitude, xend=llon, y=deploy_latitude, yend=llat, 
+                 size = n,  colour= station_group),
+             alpha = 0.5) +
+  geom_point(data = sharkov_result_df, 
+             aes(deploy_longitude, deploy_latitude, 
+                 size = sharkov_result_eigen*1000, fill = station_group),
+             shape = 21, show.legend = F) +
+  scale_fill_manual(values = cols, guide = "none") +
+  scale_colour_manual(values = cols, guide = "none") +
+  scale_size_continuous(breaks = c(2, 20, 200), limits = c(1,350), range = c(0.8,8)) +
+  labs(x = "", y = "")
+
+
+
+#### Subplot of outer ####
+#### Filter Zeebrugge ####
+df_zeeb <- df %>% 
+  select(animal_id, station_name, date_time) %>% 
+  left_join(deploy_zeeb)
+
+#### Get edges ####
+df_short <- df_zeeb %>% 
+  arrange(date_time) %>% 
+  group_by(animal_id) %>% 
+  mutate(llon=lead(deploy_longitude), llat=lead(deploy_latitude), lstation = lead(station_name), lstation_group = lead(station_group)) %>% 
+  filter(llon != deploy_longitude) %>% 
+  # filter(!is.na(llon))  %>% 
+  group_by(animal_id, station_name, station_group, deploy_longitude, deploy_latitude, lstation, lstation_group, llon, llat) %>% 
+  summarise(n=n()) %>% 
+  group_by(station_name, station_group, deploy_longitude, deploy_latitude, lstation, lstation_group, llon, llat) %>% 
+  summarise(n = sum(n))
+
+#### Format for sharkov ####
+df_sharkov = df %>% 
+  select(
+    id = animal_id,
+    time = date_hour,
+    state = station_name) %>% 
+  group_by(id, time, state) %>% 
+  dplyr::summarise() %>% 
+  as.data.frame()
+
+#### Calculate centrality ####
+sharkov_result_eigen <- sharkov(df_sharkov, return.matrix = FALSE, time_res = "1 hour", include_absence = FALSE)
+sharkov_result_df = data.frame(
+  station_name = rownames(sharkov_result_eigen),
+  sharkov_result_eigen = round(sharkov_result_eigen, digits = 2)
+)
+sharkov_result_df = sharkov_result_df %>% left_join(deploy_zeeb)
+
+#### Plot map ####
+network_map_out = base_map +
+  coord_sf(xlim = c(3.16, 3.23), ylim = c(51.35, 51.37)) +
+  theme(axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        panel.grid = element_blank(),
+        plot.margin = unit(c(0,0,0,0), "cm")) +
+  theme(legend.position=c(0.15, 0.1),
+        legend.title = element_blank(),
+        panel.border = element_rect(colour = "black", fill = NA)) +
+  geom_curve(data = filter(df_short, 
+                           station_group == "Zeebrugge_out" & 
+                             lstation_group == "Zeebrugge_out"), 
+             aes(x=deploy_longitude, xend=llon, y=deploy_latitude, yend=llat, 
+                 size = n,  colour= station_group),
+             curvature = -0.5, alpha = 0.5, show.legend = F) +
+  geom_point(data = filter(sharkov_result_df, station_group == "Zeebrugge_out"), 
+             aes(deploy_longitude, deploy_latitude, 
+                 size = sharkov_result_eigen*1000, fill = station_group),
+             shape = 21, show.legend = F) +
+  scale_fill_manual(values = cols, guide = "none") +
+  scale_colour_manual(values = cols, guide = "none") +
+  scale_size_continuous(breaks = c(2, 20, 200), limits = c(1,350), range = c(0.8,8)) +
+  labs(x = "", y = "")
+
+
+network_map_combi = network_map_out /network_map
+
+#### Save ####
+ggsave(filename = "reports/figures/Fig5_Networkmap.jpg", 
+       plot = network_map_combi,   
+       scale = 1, dpi = 600, width = 13, height = 16, units = "cm")
+
